d0261d80e0d97ec99d7e3a3c5a980ce86d8fd77b
Ismael Olguin <isc.ismael.on@gmail.com>
2023-02-14 22:43:43 -0600

    Se cambió el orden de las etiquetas Script para poder utilizar la api-key del archivo secrets.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log('hola prro')
+async function getTrendingMoviesPreview() {
+    const res = await fetch('http://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const movies = data.results;
+    console.log({data, movies});
+}
+
+getTrendingMoviesPreview();
