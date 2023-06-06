14dba1f5415739e02fbdbe0fd0cdce12dad5a954
Ismael Olguin <isc.ismael.on@gmail.com>
2023-06-05 23:49:56 -0500

    Se agregó filtro por categoría

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ async function getTrendingMoviesPreview() {
         console.log(movie.poster_path);
     });
     
-    console.log({data, movies});
+    // console.log({data, movies});
     //console.log(sinopsis);
 }
 
@@ -30,8 +30,14 @@ async function getCategories() {
         const cat = document.createElement('div');
         cat.className = 'category__slot';
         const catText = document.createTextNode(gen.name);
+        cat.addEventListener('click', () => {
+            location.hash=`#category=${gen.id}-${gen.name}`;
+            header__catTitle.innerHTML=gen.name;
+            getMovieByCategory(gen.id);
+        })
         cat.appendChild(catText);
         categories.appendChild(cat);
+        
         
     });
     
@@ -40,4 +46,26 @@ async function getCategories() {
 
 }
 
+async function getMovieByCategory(id) {
+    // const res = await fetch('http://api.themoviedb.org/3/discover/movie?with__genres=?api_key=' + API_KEY);
+    const res = await fetch(`http://api.themoviedb.org/3/discover/movie?with_genres=${id}&api_key=${API_KEY}&language=es`);
+    const data = await res.json();
 
+    const movies = data.results;
+    filterCat.innerHTML = "";
+    console.log(movies);
+
+    movies.forEach(movie => {
+        const movieCard = document.createElement('div');
+        const posterImg = document.createElement('img');
+        const titleMovie = document.createElement('h1');
+        movieCard.classList.add('filterCat__movieCard');
+        posterImg.setAttribute('alt' , movie.title);
+        posterImg.setAttribute('src' , 'https://image.tmdb.org/t/p/original' + movie.poster_path);
+        titleMovie.innerHTML = movie.title;
+        movieCard.appendChild(posterImg);
+        movieCard.appendChild(titleMovie);
+        filterCat.appendChild(movieCard);
+    });
+
+}
