e1bd1cf182776d26849e3eccabdff4dbd79c8c65
Ismael Olguin <isc.ismael.on@gmail.com>
2023-06-08 01:20:15 -0500

    Se le agrego función a la barra de búsqueda

--- src/main.js
+++ src/main.js
@@ -68,6 +68,46 @@ async function getMovieByCategory(id) {
 
 }
 
+async function getMoviesBySearch(query) {
+    // const res = await fetch(`http://api.themoviedb.org/3/discover/movie?with_genres=${id}&api_key=${API_KEY}&language=es`);
+    const res = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${API_KEY}&language=es`);
+    const data = await res.json();
+
+    const movies = data.results;
+    finder.innerHTML = "";
+
+    movies.forEach(movie => {
+        const finder__result = document.createElement('div');
+        const finder__poster = document.createElement('div');
+        const poster = document.createElement('img');
+        const finder__info = document.createElement('div');
+        const finder__title = document.createElement('h1');
+        const finder__date = document.createElement('h3');
+
+        finder__result.classList.add('finder__result');
+        finder__poster.classList.add('finder__poster');
+        poster.classList.add('description__img');
+
+        poster.setAttribute('alt' , movie.title);
+        poster.setAttribute('src' , 'https://image.tmdb.org/t/p/original' + movie.poster_path);
+
+        finder__info.classList.add('finder__info');
+        finder__title.classList.add('finder__title');
+        finder__date.classList.add('finder__date');
+        finder__title.innerHTML = movie.title;
+        finder__date.innerHTML = movie.release_date;
+
+        finder__poster.appendChild(poster);
+        finder__info.appendChild(finder__title);
+        finder__info.appendChild(finder__date);
+
+        finder__result.appendChild(finder__poster);
+        finder__result.appendChild(finder__info);
+        finder.appendChild(finder__result);
+    });
+
+}
+
 header__btnFind.addEventListener('click' , () => {
     location.hash = '#search=' + header__input.value;
 
